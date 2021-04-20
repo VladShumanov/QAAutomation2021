ba88f017da18e1eda74fa41c208dff9754729595
Vlad <morromeugene@gmail.com>
2021-04-20 17:19:00 +0300

    added comments

--- H2/H2/Program.cs
+++ H2/H2/Program.cs
@@ -5,6 +5,7 @@ namespace H2
 {
     class Program
     {
+        //concatenates an array of strings coming from the command line into one line
         private static string ConcatenationOfArguments( string[] args )
         {
             StringBuilder line = new StringBuilder( );
@@ -23,26 +24,39 @@ namespace H2
             return line.ToString( );
         }
 
+        //finds the largest substring where no character is repeated with adjacent characters
         private static int[] NoRepetitionsSideBySide( string line )
         {
-            int leftPosition = 0, rightPosition = 0, leftBoundary = 0, rightBoundary = 0;
+            //the bounds substring where no character is repeated with adjacent characters
+            int leftBoundary = 0;
+            int rightBoundary = 0;
 
+            //the bounds of the largest found substring
+            int leftPosition = 0;
+            int rightPosition = 0;
+
+            //iterates over the string from left to right
             while ( rightBoundary < line.Length - 1 )
             {
+                //compare of adjacent characters for equality
                 if ( line[rightBoundary] == line[rightBoundary + 1] )
                 {
+                    //compare the found substring with the maximum found substring
                     if ( rightBoundary - leftBoundary > rightPosition - leftPosition )
                     {
                         leftPosition = leftBoundary;
                         rightPosition = rightBoundary;
                     }
 
+                    //moving left border to find new substring
                     leftBoundary = rightBoundary + 1;
                 }
 
+                //trying to increment a substring by one character
                 rightBoundary++;
             }
 
+            //checking: the maximum substring is at the end of the string?
             if ( line.Length - 1 - leftBoundary > rightPosition - leftPosition )
             {
                 leftPosition = leftBoundary;
@@ -54,31 +68,45 @@ namespace H2
             return positions;
         }
 
+        //finds the largest substring where no character is repeated
         private static int[] NoRepetitionsInPrinciple( string line )
         {
-            int leftPosition = 0, rightPosition = 0, leftBoundary = 0, rightBoundary = 0;
+            //the bounds substring where no character is repeated with adjacent characters
+            int leftBoundary = 0;
+            int rightBoundary = 0;
 
+            //the bounds of the largest found substring
+            int leftPosition = 0;
+            int rightPosition = 0;
+
+            //iterates over the string from left to right
             while ( rightBoundary < line.Length )
             {
+                //compare the new character in the substring with the previous characters
                 for ( int i = leftBoundary; i < rightBoundary; i++ )
                 {
                     if ( line[i] == line[rightBoundary] )
                     {
+                        //compare the found substring with the maximum found substring
                         if ( rightBoundary - 1 - leftBoundary > rightPosition - leftPosition )
                         {
                             leftPosition = leftBoundary;
                             rightPosition = rightBoundary - 1;
                         }
 
+                        //to exit the loop
                         i = rightBoundary - 1;
 
+                        //moving left border to find new substring
                         rightBoundary = ++leftBoundary;
                     }
                 }
 
+                //trying to increment a substring by one character
                 rightBoundary++;
             }
 
+            //checking: the maximum substring is at the end of the string?
             if ( line.Length - 1 - leftBoundary > rightPosition - leftPosition )
             {
                 leftPosition = leftBoundary;
@@ -90,6 +118,7 @@ namespace H2
             return positions;
         }
 
+        //outputs the substring along with the number of characters in the substring
         private static void SequenceOutput( string line, int[] positions )
         {
             Console.Write( "{" );
@@ -104,6 +133,7 @@ namespace H2
         {
             if ( args.Length > 0 )
             {
+                //outputs an array of strings coming from the command line
                 for ( int i = 0; i < args.Length; i++ )
                 {
                     Console.Write( args[i] + " " );
